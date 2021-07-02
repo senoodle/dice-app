9420ba6c44bc4523464ded5fc958ffa04c1ce4c2
Sibel Eren <edaeren575@gmail.com>
2021-07-02 11:33:45 +0100

    completed dice app

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,6 +1,28 @@
 class Dice 
+
+  attr_reader :dice_amount
+  attr_reader :dice
+
+  def initialize
+    @dice_amount = 2 #default 2 to use in testing
+    @dice
+  end
+  
+
+  def amount_of_dice
+    @dice_amount
+  end
+
   def roll
-    1
+    rand(6)
+  end
+
+  def number_of_rolls
+    amount_of_dice * roll
+  end
+
+  def recorded_score
+    @dice << roll
   end
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -12,4 +12,20 @@ describe Dice do
   it 'rolling die gives number between 1 and 6' do
     expect(subject.roll).to be_between(1,6).inclusive
   end
+
+  it 'rolls multiple dice' do
+    expect(subject).to respond_to :amount_of_dice
+  end
+
+  it 'expects to roll 2 dice' do
+    expect(subject.amount_of_dice).to eq 2
+  end
+
+  it 'check score is added up' do
+    expect(subject).to respond_to :number_of_rolls
+  end
+
+  it 'get current score' do
+    expect(subject).to respond_to :recorded_score
+  end
 end
